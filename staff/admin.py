7610f1e0abf943f72e220f0e0162dc829787996e
Ankit Tiwari <ankittiwaari@gmail.com>
2025-02-28 20:53:08 +0530

    Customize staff admin display

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Staff
 
 # Register your models here.
+
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('user', 'department', 'designation')
+admin.site.register(Staff, StaffAdmin)
--- staff/models.py
+++ staff/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from departments.models import Department
 
 # Create your models here.
-# class Staff(models.Model):
-#     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-#     designation = models.CharField(max_length=100)
-#     type = models.TextChoices("Permanent", "Contract")
-#     department = models.CharField(max_length=100)
+class Staff(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    designation = models.CharField(max_length=100)
+    type = models.TextChoices("Permanent", "Contract")
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        ordering = ['user__username']
+        verbose_name_plural = 'Staff members'
